ccde2daedba6993e644f7c1f242e0ed049818b71
Scott Smitelli <scott@smitelli.com>
2013-10-12 13:45:12 -0400

    Code style updates

--- include/overlay.php
+++ include/overlay.php
@@ -1,43 +1,43 @@
-<?php if (!defined('GOOD_INIT')) die("Go away."); ?>
+<?php if (!defined('GOOD_INIT')) die('Go away.'); ?>
 
 <!-- Nothing loading? Maybe the site you're trying to view is setting a
      X-Frame-Options header. Security bastards ruin all the fun. -->
 
 <div id="overlay-box" class="flipped"></div>
-<iframe id="content-box" src="<?php echo htmlentities($url); ?>"></iframe>
+    <iframe id="content-box" src="<?php echo htmlentities($url); ?>"></iframe>
 
-<script>
-  var xPos   = 0;
-  var yPos   = 0;
-  var xDelta = (Math.random() * 5) + 5;
-  var yDelta = (Math.random() * 5) + 5;
+    <script>
+        var xPos   = 0,
+            yPos   = 0,
+            xDelta = (Math.random() * 5) + 5,
+            yDelta = (Math.random() * 5) + 5;
 
-  setInterval(function() {
-    // TODO: Really only changes on load and after window resizes
-    var xMax = $(window).width()  - $('#overlay-box').width();
-    var yMax = $(window).height() - $('#overlay-box').height();
+        setInterval(function () {
+            // TODO: Really only changes on load and after window resizes
+            var xMax = $(window).width()  - $('#overlay-box').width(),
+                yMax = $(window).height() - $('#overlay-box').height();
 
-    xPos += xDelta;
-    yPos += yDelta;
+            xPos += xDelta;
+            yPos += yDelta;
 
-    if (xPos < 0) {
-      xPos   = 0;
-      xDelta = Math.abs(xDelta);
-      $('#overlay-box').addClass('flipped');
-    } else if (xPos > xMax) {
-      xPos   = xMax;
-      xDelta = -Math.abs(xDelta);
-      $('#overlay-box').removeClass('flipped');
-    }
+            if (xPos < 0) {
+                xPos   = 0;
+                xDelta = Math.abs(xDelta);
+                $('#overlay-box').addClass('flipped');
+            } else if (xPos > xMax) {
+                xPos   = xMax;
+                xDelta = -Math.abs(xDelta);
+                $('#overlay-box').removeClass('flipped');
+            }
 
-    if (yPos < 0) {
-      yPos   = 0;
-      yDelta = Math.abs(yDelta);
-    } else if (yPos > yMax) {
-      yPos   = yMax;
-      yDelta = -Math.abs(yDelta);
-    }
+            if (yPos < 0) {
+                yPos   = 0;
+                yDelta = Math.abs(yDelta);
+            } else if (yPos > yMax) {
+                yPos   = yMax;
+                yDelta = -Math.abs(yDelta);
+            }
 
-    $('#overlay-box').css({ left: xPos+'px', top: yPos+'px' });
-  }, 33);  //1000/33 = ~30fps
+        $('#overlay-box').css({left : xPos + 'px', top : yPos + 'px'});
+    }, 33);  // 1000/33 = ~30fps
 </script>
