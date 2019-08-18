6bd073160b503b71f58a6c6aa90e5d7d756799b6
Scott Smitelli <scott@smitelli.com>
2019-08-18 10:32:47 -0400

    I do not care about Google Analytics anymore

--- public/index.php
+++ public/index.php
@@ -56,22 +56,6 @@
         require dirname(__FILE__) . '/../include/landing-page.php';
     }
 
-    if ($options['ga_identifier']) {
-
-?>
-    <script>
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', '<?php echo addslashes($options['ga_identifier']); ?>'], ['_trackPageview']);
-        (function () {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-    </script>
-<?php
-
-    }
-
 ?>
 </body>
 </html>
